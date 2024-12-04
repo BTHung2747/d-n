6875a23b8e7ed62e4ab0bc8136b1c38e293a3ae5
BTHung2747 <165037475+BTHung2747@users.noreply.github.com>
2024-12-04 23:20:06 +0700

    Delete routes directory

--- routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-
-// Các route liên quan đến bình luận
-router.get('/', commentController.getComments);
-router.post('/', commentController.createComment);
-
-module.exports = router;
